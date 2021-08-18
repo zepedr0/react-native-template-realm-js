698f82f7ca204b56b9220f329519dbd84a0f6172
LJ <81748770+elle-j@users.noreply.github.com>
2021-08-18 18:19:58 +0200

    Add Realm functionality

--- App.js
+++ App.js
@@ -1,112 +1,184 @@
-import React, { useState } from 'react';
-import { View, TextInput, Pressable, Text, StyleSheet, SafeAreaView, FlatList } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { SafeAreaView, View, Text, TextInput, Pressable, FlatList, StyleSheet } from 'react-native';
 import Realm from 'realm';
 import openURLInBrowser from 'react-native/Libraries/Core/Devtools/openURLInBrowser';
-class Task {
-  constructor({ id = new Realm.BSON.ObjectId(), description, isComplete = false }) {
-    this._id = id;
-    this.description = description;
 
-    if (isComplete)
-      this.isComplete = isComplete;
-  }
-
-  static schema = {
-    name: 'Task',
-    primaryKey: '_id',
-    properties: {
-      _id: 'objectId',
-      description: 'string',
-      isComplete: { type: 'bool', default: false }
-    }
-  };
-}
-
-const configuration = {
-  schema: [Task.schema],
-  // deleteRealmIfMigrationNeeded: true,
-};
-
-const realm = new Realm(configuration);
+import Task from './Task';
 
 export default function App() {
-
-  const [text, setText] = useState('');
+  const [newTaskDescription, setNewTaskDescription] = useState('');
   const [tasks, setTasks] = useState([]);
+  const realmRef = useRef(null);
+  const subscriptionRef = useRef(null);
+
+  useEffect(() => {
+    openRealm();
+
+    // Return a cleanup callback to close the realm to prevent memory leaks
+    return closeRealm;
+  }, []);
+
+  const openRealm = () => {
+    try {
+      // Open a local realm file with the schemas
+      const config = {
+        schema: [Task.schema],
+        // deleteRealmIfMigrationNeeded: true
+      };
+
+      const realm = new Realm(config);
+      realmRef.current = realm;
+
+      // When querying a realm to find objects (e.g. realm.objects('Tasks')) the result we get back
+      // and the objects in it are "live" and will always reflect the latest state.
+      const tasks = realm.objects('Task');
+      if (tasks?.length)
+        setTasks(tasks);
+
+      // Live queries and objects emit notifications when something has changed that we can listen for.
+      subscriptionRef.current = tasks;
+      tasks.addListener((/*collection, changes*/) => {
+        // If wanting to handle deletions, insertions, and modifications differently
+        // you can access them through the two arguments. (Always handle them in the
+        // following order: deletions, insertions, modifications)
+        // e.g. changes.insertions.forEach((index) => console.log('Inserted task: ', collection[index]));
+
+        // By querying the objects again, we get a new reference to the Result and triggers
+        // a rerender by React. Setting the tasks to either 'tasks' or 'collection' (from the
+        // argument) will not trigger a rerender since it is the same reference
+        setTasks(realm.objects('Task'));
+      });
+    }
+    catch (err) {
+      console.error('Error opening realm: ', err.message);
+    }
+  };
+
+  const closeRealm = () => {
+    const subscription = subscriptionRef.current;
+    subscription?.removeAllListeners();
+    subscriptionRef.current = null;
+
+    const realm = realmRef.current;
+    // If having listeners on the realm itself, also remove them using:
+    // realm?.removeAllListeners();
+    realm?.close();
+    realmRef.current = null;
+    setTasks([]);
+  };
 
   const handleAddTask = () => {
-    if (text === '')
+    if (!newTaskDescription)
       return;
+    
+    // Everything in the function passed to "realm.write" is a transaction and will
+    // hence succeed or fail together. A transcation is the smallest unit of transfer
+    // in Realm so we want to be mindful of how much we put into one single transaction
+    // and split them up if appropriate (more commonly seen server side). Since clients
+    // may occasionally be online during short time spans we want to increase the probability
+    // of sync participants to successfully sync everything in the transaction, otherwise
+    // no changes propagate and the transaction needs to start over when connectivity allows.
+    const realm = realmRef.current;
+    realm?.write(() => {
+      realm?.create('Task', new Task({ description: newTaskDescription }));
+    });
 
-    setTasks([...tasks, { id: tasks.length, description: text, isComplete: false }]);
-    setText('');
-  }
+    setNewTaskDescription('');
+  };
 
-  const handleToggleTask = (id) => {
-    setTasks(tasks.map((task) => task.id === id
-      ? { id, description: task.description, isComplete: !task.isComplete }
-      : task
-    ));
-  }
+  const handleToggleTask = (task) => {
+    const realm = realmRef.current;
+    realm?.write(() => {
+      // Normally when updating a record in a NoSQL or SQL database, we have to type
+      // a statement that will later be interpreted and used as instructions for how
+      // to update the record. But in RealmDB, the objects are "live" because they are
+      // actually referencing the object's location in memory on the device (memory mapping).
+      // So rather than typing a statement, we modify the object directly by changing
+      // the property values. If the changes adhere to the schema, Realm will accept
+      // this new version of the object and wherever this object is being referenced
+      // locally will also see the changes "live".
+      task.isComplete = !task.isComplete;
+    });
+  };
 
-  const handleDeleteTask = (id) => {
-    const newTasks = tasks.filter((task) => task.id !== id);
-    setTasks(newTasks);
-  }
+  const handleDeleteTask = (task) => {
+    const realm = realmRef.current;
+    realm?.write(() => {
+      realm?.delete(task);
+    });
+  };
 
   return (
     <SafeAreaView style={styles.screen}>
       <View style={styles.form}>
-        <TextInput style={styles.textInput} placeholder="Task name to add" onChangeText={setText} value={text} autoCorrect={false} />
-        <Pressable style={styles.submit} onPress={handleAddTask}>
+        <TextInput
+          value={newTaskDescription}
+          placeholder='Task name to add'
+          onChangeText={setNewTaskDescription}
+          autoCorrect={false}
+          style={styles.textInput}
+        />
+        <Pressable
+          onPress={handleAddTask}
+          style={styles.submit}
+        >
           <Text style={styles.icon}>
             ＋
           </Text>
         </Pressable>
       </View>
-      {tasks.length === 0 ? (
-        <View style={styles.content}>
-          <Text style={styles.paragraph}>
-            Welcome to the Realm React Native TypeScript Template
-          </Text>
-          <Text style={styles.paragraph}>
-           Start adding a task at the form on top of the screen to see how they are created in Realm and update the UI. You can also change a task status or remove it from the tasks list.
-          </Text>
-          <Text style={styles.paragraph}>
-            You can find more information about the React Native Realm SDK in:
-          </Text>
-          <Pressable onPress={() => openURLInBrowser('https://docs.mongodb.com/realm/sdk/react-native/')}>
-            <Text style={[styles.paragraph, styles.link]}>
-              docs.mongodb.com/realm/sdk/react-native
+      {(tasks.length === 0)
+        ? (
+          <View style={styles.content}>
+            <Text style={styles.paragraph}>
+              Welcome to the Realm React Native TypeScript Template
             </Text>
-          </Pressable>
-        </View>) :
-        (<View style={styles.content}>
-          <FlatList
-            style={styles.flatList}
-            data={tasks}
-            keyExtractor={task => task.id}
-            renderItem={({ item }) => (
-              <View style={styles.task}>
-                <Pressable style={[styles.taskStatus, item.isComplete && styles.completedStatus]} onPress={() => handleToggleTask(item.id)}>
-                  <Text style={[styles.icon, item.isComplete && styles.checkmark]}>
-                    {item.isComplete ? '✓' : '-'}
-                  </Text>
-                </Pressable>
-                <View style={styles.taskDescriptionContainer}>
-                  <Text style={styles.taskDescription} numberOfLines={1} >
-                    {item.description}
-                  </Text>
+            <Text style={styles.paragraph}>
+              Start adding a task at the form on top of the screen to see how they are created in Realm and update the UI. You can also change a task status or remove it from the tasks list.
+            </Text>
+            <Text style={styles.paragraph}>
+              You can find more information about the React Native Realm SDK in:
+            </Text>
+            <Pressable onPress={() => openURLInBrowser('https://docs.mongodb.com/realm/sdk/react-native/')}>
+              <Text style={[styles.paragraph, styles.link]}>
+                docs.mongodb.com/realm/sdk/react-native
+              </Text>
+            </Pressable>
+          </View>
+        ) : (
+          <View style={styles.content}>
+            <FlatList
+              data={tasks}
+              keyExtractor={(item) => item._id.toString()}
+              renderItem={({ item }) => (
+                <View style={styles.task}>
+                  <Pressable
+                    onPress={() => handleToggleTask(item)}
+                    style={[styles.taskStatus, item.isComplete && styles.completedStatus]}
+                  >
+                    <Text style={[styles.icon, item.isComplete && styles.checkmark]}>
+                      {item.isComplete ? '✓' : '◯'}
+                    </Text>
+                  </Pressable>
+                  <View style={styles.taskDescriptionContainer}>
+                    <Text style={styles.taskDescription} numberOfLines={1} >
+                      {item.description}
+                    </Text>
+                  </View>
+                  <Pressable
+                    onPress={() => handleDeleteTask(item)}
+                    style={styles.deleteButton}
+                  >
+                    <Text style={styles.deleteText}>
+                      Delete
+                    </Text>
+                  </Pressable>
                 </View>
-                <Pressable style={styles.deleteButton} onPress={() => handleDeleteTask(item.id)}>
-                  <Text style={styles.deleteText}>
-                    Delete
-                  </Text>
-                </Pressable>
-              </View>
-            )}
-          />
-        </View>)
+              )}
+              style={styles.flatList}
+            />
+          </View>
+        )
       }
     </SafeAreaView>
   );
@@ -127,6 +199,8 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.7,
     shadowRadius: 3
+
+    // TODO: ADD FOR ANDROID
   },
   textInput: {
     flex: 1,
@@ -155,7 +229,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     justifyContent: 'center',
   },
-  flatList : {
+  flatList: {
     marginTop: 20
   },
   paragraph: {
@@ -209,7 +283,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     fontSize: 15
   },
-  checkmark : {
+  checkmark: {
     fontSize: 15
   }
 });
---
+++ Task.js
@@ -0,0 +1,20 @@
+import { BSON } from 'realm';
+
+export default class Task {
+  constructor({ id = new BSON.ObjectId(), description, isComplete = false }) {
+    this._id = id;
+    this.description = description;
+    this.isComplete = isComplete;
+  }
+
+  // To use a class as a Realm object type, define the object schema on the static property "schema".
+  static schema = {
+    name: 'Task',
+    primaryKey: '_id',
+    properties: {
+      _id: 'objectId',
+      description: 'string',
+      isComplete: { type: 'bool', default: false }
+    }
+  };
+}
