dc90af3e0ace8b7e88c2cf5578400d4566a6db70
José Martins <zepedromm1998@gmail.com>
2021-08-18 12:16:09 +0100

    Created Task schema

--- App.js
+++ App.js
@@ -1,4 +1,25 @@
 import React from 'react';
+import Realm from 'realm';
+
+class Task {
+  constructor({ id = new Realm.BSON.ObjectId(), description, isComplete = false }) {
+    this._id = id;
+    this.description = description;
+
+    if (isComplete)
+      this.isComplete = isComplete;
+  }
+
+  static schema = {
+    name: 'Task',
+    primaryKey: '_id',
+    properties: {
+      _id: 'objectId',
+      description: 'string',
+      isComplete: {type: 'bool', default: false}
+    }
+  };
+}
 
 export default function App() {
   return null;
