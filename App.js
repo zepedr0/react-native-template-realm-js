53f1f690e35344124e6d2f37140d3335b3bfe84c
José Martins <zepedromm1998@gmail.com>
2021-08-18 14:10:08 +0100

    Added task adder form

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View, TextInput, Pressable, Text, StyleSheet } from 'react-native';
 import Realm from 'realm';
 
 class Task {
@@ -16,7 +17,7 @@ class Task {
     properties: {
       _id: 'objectId',
       description: 'string',
-      isComplete: {type: 'bool', default: false}
+      isComplete: { type: 'bool', default: false }
     }
   };
 }
@@ -29,5 +30,57 @@ const configuration = {
 const realm = new Realm(configuration);
 
 export default function App() {
-  return null;
+  return (
+    <View style={styles.screen}>
+      <View style={styles.form}>
+        <TextInput style={styles.textInput} placeholder="Task name to add" />
+        <Pressable style={styles.submit}>
+          <Text style={styles.icon}>
+            ＋
+          </Text>
+        </Pressable>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: '#2A3642'
+  },
+  form: {
+    flexDirection: 'row',
+    marginTop: 60,
+    paddingHorizontal: 20,
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 4
+    },
+    shadowOpacity: 0.7,
+    shadowRadius: 3
+  },
+  textInput: {
+    flex: 1,
+    padding: 15,
+    borderRadius: 5,
+    backgroundColor: '#FFFFFF',
+    fontSize: 15,
+  },
+  submit: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 20,
+    borderRadius: 5,
+    backgroundColor: '#6E60F9',
+    width: 50,
+    height: 50
+  },
+  icon: {
+    color: '#FFFFFF',
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
+});
